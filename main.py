e6ac8343bc0f1d12f764c89cbeb189a0df7a0dea
swishyy <68352336+kakajuro@users.noreply.github.com>
2025-12-18 23:58:02 +0000

    yagmail added

--- main.py
+++ main.py
@@ -1,27 +1,18 @@
-import os
 from dotenv import load_dotenv
-import smtplib
-from email.mime.text import MIMEText
+import os
+import datetime
+import yagmail
 
 load_dotenv()
 
-subject = "This is a test subject"
+subject = f'Extension Update: {datetime.datetime.now().strftime("%x")}'
 body = "This is a test email!!"
 sender = os.environ.get("EMAIL_2")
 recipients = [os.environ.get("EMAIL_2")]
 password = os.environ.get("EMAIL_PASSWORD_2")
 
 
-def send_email(subject, body, sender, recipients, password):
-    msg = MIMEText(body)
-    msg['Subject'] = subject
-    msg['From'] = sender
-    msg['To'] = ', '.join(recipients)
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
-       smtp_server.login(sender, password)
-       smtp_server.sendmail(sender, recipients, msg.as_string())
-    print("Message sent!")
-
-
-send_email(subject, body, sender, recipients, password)
+with yagmail.SMTP({sender: "Hello"}, password) as yag:
+  yag.send(to = {sender: sender}, subject=subject, contents=body)
+print("Sent!")
 
