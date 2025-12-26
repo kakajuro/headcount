d8d4e66ce47ebebfde1b779e42d5afcdee24b3eb
swishyy <68352336+kakajuro@users.noreply.github.com>
2025-12-26 17:50:46 +0000

    Edited python script for testing

--- scripts/weekly.py
+++ scripts/weekly.py
@@ -1,56 +1,32 @@
 from dotenv import load_dotenv
-import requests
 import os
 import datetime
+import requests
 import yagmail
 
 load_dotenv()
 
-api_url = ""
-
-if int(os.environ.get("IS_PRODUCTION")) == 0:
-  api_url = "http://localhost:3000"
-else:
-  api_url = os.environ.get("PRODUCTION_API_URL")
-
-# Email setup
-subject = f'Extension Update: {datetime.datetime.now().strftime("%x")}'
+api_url = os.environ.get("PRODUCTION_API_URL") if int(os.environ.get("IS_PRODUCTION")) else "http://localhost:3000"
 sender = os.environ.get("EMAIL")
-recipients = [os.environ.get("EMAIL")]
 password = os.environ.get("EMAIL_PASSWORD")
-body = f"""Extension usercounts for this week: \n"""
 
-# API call
-print("Fetching extension data")
-response = requests.get(f"{api_url}/counts/weekly").json()
+subject = f'Extension Update: {datetime.datetime.now().strftime("%x")}'
+body = "Extension usercounts for this week:\n"
 
-print("Formatting update email")
-for app in response:
-  totalUsers = app["usercountChrome"] + app["usercountFirefox"] + app["usercountEdge"]
-  sign = ""
-  daysCount = ""
+print("Fetching extension data", flush=True)
+response = requests.get(f"{api_url}/counts/weekly", timeout=10)
+response.raise_for_status()
+data = response.json()
 
-  if app["userDifference"] >= 0:
-    sign = "+"
-  elif app["userDifference"] < 0:
-    sign = "-"
+print("Formatting update email", flush=True)
+for app in data:
+    totalUsers = app["usercountChrome"] + app["usercountFirefox"] + app["usercountEdge"]
+    sign = "+" if app["userDifference"] >= 0 else "-"
+    daysCount = "1 week ago" if app["dayDifference"] == 7 else f"{app['dayDifference']} days ago"
 
-  if app["dayDifference"] == 7:
-    daysCount = "1 week ago"
-  else:
-    daysCount = f"{app["dayDifference"]} days ago"
+    body += f"{app['name']} ({app['shortname']}) has {totalUsers} users ({sign}{app['userDifference']} from {daysCount})\n"
 
-  dataString = f"""
-  {app["name"]} ({app["shortname"]}) has {totalUsers} users
-  ({sign}{app["userDifference"]} from {daysCount})
-  """
-
-  body += dataString
-
-print("Sending update email")
-
-with yagmail.SMTP({sender: "Hello"}, password, port=587) as yag:
-  yag.send(to = {sender: sender}, subject=subject, contents=body)
-
-print("Sent update to inbox!")
-
+print("Sending update email", flush=True)
+yag = yagmail.SMTP(user=sender, password=password, port=587, smtp_starttls=True, smtp_ssl=False, timeout=10)
+yag.send(to=sender, subject=subject, contents=body)
+print("Sent update to inbox!", flush=True)
