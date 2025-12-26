837c74ad8a47242cfa9d73a47860a5e66222e508
swishyy <68352336+kakajuro@users.noreply.github.com>
2025-12-26 18:58:25 +0000

    Switch email service to mailjet

--- scripts/weekly.py
+++ scripts/weekly.py
@@ -2,13 +2,18 @@ from dotenv import load_dotenv
 import os
 import datetime
 import requests
-import yagmail
+from mailjet_rest import Client
 
 load_dotenv()
 
 api_url = os.environ.get("PRODUCTION_API_URL") if int(os.environ.get("IS_PRODUCTION")) else "http://localhost:3000"
 sender = os.environ.get("EMAIL")
 password = os.environ.get("EMAIL_PASSWORD")
+
+MJ_PUBLIC=os.environ.get("MJ_PUBLIC")
+MJ_PRIVATE=os.environ.get("MJ_PRIVATE")
+EMAIL=os.environ.get("EMAIL")
+mailjet = Client(auth=(MJ_PUBLIC, MJ_PRIVATE), version="v3.1")
 
 subject = f'Extension Update: {datetime.datetime.now().strftime("%x")}'
 body = "Extension usercounts for this week:\n"
@@ -27,6 +32,21 @@ for app in data:
     body += f"{app['name']} ({app['shortname']}) has {totalUsers} users ({sign}{app['userDifference']} from {daysCount})\n"
 
 print("Sending update email", flush=True)
-yag = yagmail.SMTP(user=sender, password=password, port=587, smtp_starttls=True, smtp_ssl=False, timeout=10)
-yag.send(to=sender, subject=subject, contents=body)
-print("Sent update to inbox!", flush=True)
+
+data = {
+    'Messages': [
+        {
+            "From": {"Email": EMAIL, "Name": "Extension Update"},
+            "To": [{"Email": EMAIL, "Name": "Recipient"}],
+            "Subject": subject,
+            "TextPart": body
+        }
+    ]
+}
+
+result = mailjet.send.create(data=data)
+
+if result.status_code == 200:
+    print("Sent update to inbox!", flush=True)
+
+
